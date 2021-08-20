9092b95a86251ff996afbfc2bfbeca7b3e0ef42d
Amphy-nick <D_burkovskui@ukr.net>
2021-08-20 16:28:55 +0300

    сделал ссылки на  элементы

---
+++ index.js
@@ -0,0 +1,8 @@
+const refs = {
+timerId: document.querySelector('#timer-1'),
+timerDays:  document.querySelector('span[data-value="days"]'),
+timerHours:  document.querySelector('span[data-value="hours]'),
+timerMinutes:  document.querySelector('span[data-value="mins"]'),
+timerSeconds:  document.querySelector('span[data-value="secs"]'),
+}
+console.log(refs.timerId)
